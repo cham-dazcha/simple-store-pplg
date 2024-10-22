611c2a3c26c1c727f489e88d86f27df637f5e593
Xyutori <muhamatjayakusuma2008@gmail.com>
2024-10-22 14:49:17 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('admin', function (){
+    return view('admin.dashboard');
+});
+Route::get('login', function (){
+    return view('admin.login');
+});
